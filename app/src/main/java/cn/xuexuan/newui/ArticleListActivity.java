170a662ff01d2834e5bd3a16debe38b713c6d568
Allen <836414757@qq.com>
2018-09-05 13:51:05 +0800

    上拉加载更多功能

--- app/src/main/java/cn/xuexuan/newui/ArticleListActivity.java
+++ app/src/main/java/cn/xuexuan/newui/ArticleListActivity.java
@@ -9,36 +9,57 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class ArticleListActivity extends Activity {
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class ArticleListActivity extends Activity implements BaseQuickAdapter.RequestLoadMoreListener {
 
     private RecyclerView rlv;
-    //private ImageView iv_avatar;
-    private RelativeLayout rl_top;
     private AppBarLayout mAppBarLayout;
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private LinearLayout ll_toolbar_content;
+    private LinearLayout ll_main_content;
 
     private ArticleListAdapter mArticleListAdapter;
     private List<String> mList;
+    float screenW;
+    float toolbarHeight;
+    float initHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
+        screenW = getResources().getDisplayMetrics().widthPixels;
+        toolbarHeight = getResources().getDimension(R.dimen.tool_bar_height);
+        initHeight = getResources().getDimension(R.dimen.app_bar_height);
+
         findView();
         initRecycle();
         initListener();
+        ll_toolbar_content.setAlpha(0);
     }
 
     private void initListener() {
         mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                Log.d("wbl", "AppBarLayout verticalOffset :" + verticalOffset);
+                float scale = 1.0f - (-verticalOffset) / (initHeight - toolbarHeight);
+                Log.d("wbl", "scale :" + scale);
+                ll_toolbar_content.setAlpha(1 - scale);
+                ll_main_content.setAlpha(scale);
             }
         });
     }
@@ -54,13 +75,32 @@ public class ArticleListActivity extends Activity {
         rlv.setAdapter(mArticleListAdapter);
         rlv.setLayoutManager(new LinearLayoutManager(this));
         rlv.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        mArticleListAdapter.setOnLoadMoreListener(this, rlv);
     }
 
     private void findView() {
         rlv = (RecyclerView) findViewById(R.id.recyclerView);
         //iv_avatar = (ImageView) findViewById(R.id.iv_avatar);
-        rl_top = (RelativeLayout) findViewById(R.id.top_relative);
         mAppBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
         mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapse_layout);
+        ll_toolbar_content = (LinearLayout) findViewById(R.id.ll_toolbar_content);
+        ll_main_content = (LinearLayout) findViewById(R.id.ll_main_content);
+    }
+
+    @Override
+    public void onLoadMoreRequested() {
+        Observable.timer(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(@NonNull Long aLong) throws Exception {
+                        mArticleListAdapter.loadMoreComplete();
+                        for (int i = 0; i < 5; i++) {
+                            mList.add(null);
+                        }
+                        mArticleListAdapter.notifyDataSetChanged();
+                    }
+                });
     }
 }
